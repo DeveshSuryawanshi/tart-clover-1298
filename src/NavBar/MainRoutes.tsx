8a7198bc31b065274a67f2c24aa0236d4578f0ff
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2023-07-18 21:47:11 +0530

    Structure Done

--- src/NavBar/MainRoutes.tsx
+++ src/NavBar/MainRoutes.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Blogs } from "../pages/Blogs/Blogs";
+import { Calculator } from "../pages/Calculator/Calculator";
+import { Contact } from "../pages/Contact/Contact";
+import { Faq } from "../pages/FAQ/Faq";
+import { HomePage } from "../pages/HomePage/HomePage";
+import { Login } from "../pages/Login/Login";
 
 export const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home Page</h1>}/>
-      <Route path="/about" element={<h1>About</h1>}/>
-      <Route path="/login" element={<h1>Login</h1>}/>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/calculator" element={<Calculator/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/blogs" element={<Blogs/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/faq" element={<Faq/>}/>
     </Routes>
   );
 };
--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 export const NavBar = () => {
   return (
     <div>
-        <Link to='/'>Home page</Link>
-        <Link to='/about'>About</Link>
+        <Link to='/'>Homepage</Link>
+        <Link to='/calculator'>Calculator</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/blogs'>Blogs</Link>
+        <Link to='/faq'>Faq</Link>
         <Link to='/login'>Login</Link>
     </div>
   )
