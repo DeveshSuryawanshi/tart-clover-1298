4e8623a7bf551333eaf07b832805dff4a81b91ad
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2023-07-18 18:26:05 +0530

    C-W

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello this is a React app and its working fine</h1>
+    </div>
+  );
+}
+
+export default App;
