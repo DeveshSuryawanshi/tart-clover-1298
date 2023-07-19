077e82ba0cf6b5ff14735f7091fc9c7ab2e5595c
Upendra Pal <up06072000@gmail.com>
2023-07-19 21:13:15 +0530

    Day-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { MainRoutes } from './NavBar/MainRoutes';
 import {NavBar} from "./NavBar/NavBar";
+// import ReusebaleCalculator from './pages/Calculator/ReusebaleCalculator';
+import SaleryTex from './pages/Calculator/SaleryTex';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <MainRoutes/>
+      <SaleryTex/>
+
     </div>
   );
 }
