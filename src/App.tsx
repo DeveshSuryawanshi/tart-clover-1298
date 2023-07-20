9eefd53a5b4eae5adb7a36b54692945d29049d5e
Upendra Pal <up06072000@gmail.com>
2023-07-20 22:34:32 +0530

    Day-3 Task Complete

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ function App() {
     <div className="App">
       <NavBar/>
       <MainRoutes/>
-      <SaleryTex/>
-
     </div>
   );
 }
--- src/pages/Calculator/ReusiblerightContent.tsx
+++ src/pages/Calculator/ReusiblerightContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { styled } from "styled-components";
 
 const ReusiblerightContent = () => {
   return (
-    <div className="right-side-content">
+    <DIV >
       <div className="dummy-image">
         {/* -- */}
         <div>
@@ -50,8 +51,54 @@ const ReusiblerightContent = () => {
           GET STARTED
         </Link>
       </div>
-    </div>
+    </DIV>
   );
 };
 
 export default ReusiblerightContent;
+const DIV= styled.div`
+     font-size: 16px;
+    border: 0px solid red;
+    text-align: start;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    font-family: "Droid Sans", Arial, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    color: #4d4d4d;
+
+    .dummy-image {
+    display: flex;
+    border: 0px solid red;
+    /* justify-content: space-around; */
+    align-items: center;
+  }
+  .get-start-btn {
+    padding: 0.6rem 0.8rem;
+    color: white;
+    background-color: #444444;
+    box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%),
+      0 1px 5px 0 rgb(0 0 0 / 20%);
+    border-radius: 2px;
+    border: 2px solid #444444;
+    font-family: "Montserrat", Arial, sans-serif;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: #4d4d4d;
+    font-weight: bold;
+    font-family: "Montserrat", Arial, sans-serif;
+  }
+  h1 {
+    font-size: 1.8rem;
+  }
+  h2 {
+    font-size: 1.5rem;
+  }
+`
