769ced4d118dd2b7a7e954c949f339a80c4b8ba6
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2023-07-23 18:59:46 +0530

    New Updates

--- src/NavBar/MainRoutes.tsx
+++ src/NavBar/MainRoutes.tsx
@@ -9,9 +9,8 @@ import BlogPage from '../pages/Blogs/BlogPage';
 import { Login } from "../pages/Login/Login";
 import HomePage from "../pages/HomePage/HomePage";
 import Taxrefund from "../pages/Calculator/TaxRefund";
-import Admin from "../pages/Admin/Admin";
+// import Admin from "../pages/Admin/Admin";
 import Register from "../pages/Login/Register";
-import BlogForm from "../pages/Admin/BlogForm";
 
 export const MainRoutes = () => {
   return (
@@ -19,25 +18,11 @@ export const MainRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/calculator" element={<Calculator />} />
       <Route path="/contact" element={<Contact />} />
-
-      <Route path="/admin" element={<Admin />} />
-<Route path="/blogForm" element={<BlogForm/>}/>
- 
+      {/* <Route path="/blogs" element={<Admin />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/faq" element={<Faq />} />
       <Route path="/blogs" element={<BlogPage />} />
       {/* Calculator Routes */}
-     
-      <Route path="/SalaryTax" element={<SaleryTex />} />
-      <Route path="/TaxRefund" element={""} />
-      <Route path="/RetirementLumpSum" element={""} />
-      <Route path="/TravelDeduction" element={""} />
-      <Route path="/CapitalGainsTax" element={""} />
-      <Route path="/MedicalAidCredits" element={""} />
-      <Route path="/WearTear" element={""} />
-      <Route path="/RetirementSavings" element={""} />
-      <Route path="/HomeofficeCalculator" element={<HomeOfficeCalculator />} />
-
       <Route path="/SalaryTax" element={<SaleryTex />} />
       <Route path="/TaxRefund" element={<Taxrefund />} />
       <Route path="/RetirementLumpSum" element={""} />
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import LapImg from "../HomePage/Screenshot (371).png"
 import HomePageImg from "../image/login.jpg"
-import youtube from "../HomePage/Youtube-removebg-preview.png"
+// import HomePageImg from "./Homepage.png"
 import  "./HomePage.css"
 
 import { useState } from "react";
 const HomePage = () => {
- 
-const [play,setplay]=useState(false)
- 
+  const [formData, setFormData] = useState({
+    input1: "",
+    input2: "",
+    input3: "",
+    input4: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    alert ("SignUp SuccesFully")
+  };
   return (
     <>
       <div className="container gradient-background ">
         <div className="img-container">
-          <img width="100%" src={HomePageImg} alt="homepageimg" />
+          <img width="100%" className="styles-img" src={HomePageImg} alt="homepageimg" />
           <div className="text-overlay">
             <h3 className="h3tag">
               THE FASTEST, EASIEST WAY
@@ -21,72 +37,21 @@ const [play,setplay]=useState(false)
             </h3>
           </div>
         </div>
-        {/* <div className="form-container">
-          <h2 className="h3tag">
-            Sign up for the easy
-            <br /> way to do tax returns.
-          </h2>
-          <p className="ptag">
-            Sign up for free and only pay as submit your tax
-          </p>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="input1"
-              value={formData.input1}
-              onChange={handleInputChange}
-              placeholder="Name & Surname"
-            />
-            <input
-              type="email"
-              name="input2"
-              value={formData.input2}
-              onChange={handleInputChange}
-              placeholder="Email Address"
-            />
-            <input
-              type="number"
-              name="input3"
-              value={formData.input3}
-              onChange={handleInputChange}
-              placeholder="Contact No"
-            />
-            <select
-              name="input4"
-              value={formData.input4}
-              onChange={handleInputChange}
-              placeholder="Input 4"
-            >
-              <option value="">Type of Tax</option>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </select>
-            <button type="submit">Start My Tax Return</button>
-          </form>
-          <p className="ptag">
-            By submitting this form I agree to TaxTim's Terms.
-          </p>
-        </div> */}
 
-<div className="form-container">
- {play?<iframe width="370" height="251" src="https://www.youtube.com/embed/nAFLsldyJfs" title="Have you filed your tax return? This neat tool could maximize your SARS refund" frameborder="0" 
- allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-  allowfullscreen></iframe>:<img style={{width:"370px", height:"201px",marginTop:"20px"}} src={youtube} alt="youtub" onClick={()=>setplay(true)} />}
-
-
-</div>
-
+        <div className="form-container">
+          <iframe width="558" height="314" src="https://www.youtube.com/embed/nAFLsldyJfs" title="Have you filed your tax return? This neat tool could maximize your SARS refund" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
         {/* seconde section */}
 
         <div className="section2">
           <div className="img-container2">
-            <img width="355px" style={{borderRadius:"20px",border:"1px solid green"}} src={LapImg} alt=""  />
+            <img width="300px" src={"https://media.taxtim.com/images/tim_full.svg"} alt=""  />
+            <h1 className="imgLogo imgLogo2">Save Tax</h1>
           </div>
           <div className="how-does">
             <h2 className="h3tag">How does Tax Tim Work?</h2>
             <h3 className="h3tag">It's quite easy actually!</h3>
-            <h5>
+            <h5 className="styled-h5">
               Let's have a chat. After answering my simple questions I'll
               complete your tax return and file <br />
               it directly with SARS for your maximum possible refund!
@@ -272,3 +237,60 @@ const [play,setplay]=useState(false)
 };
 
 export default HomePage;
+
+
+
+
+
+
+
+
+
+
+
+
+          {/* <h2 className="h3tag">
+            Sign up for the easy
+            <br /> way to do tax returns.
+          </h2>
+          <p className="ptag">
+            Sign up for free and only pay as submit your tax
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="input1"
+              value={formData.input1}
+              onChange={handleInputChange}
+              placeholder="Name & Surname"
+            />
+            <input
+              type="email"
+              name="input2"
+              value={formData.input2}
+              onChange={handleInputChange}
+              placeholder="Email Address"
+            />
+            <input
+              type="number"
+              name="input3"
+              value={formData.input3}
+              onChange={handleInputChange}
+              placeholder="Contact No"
+            />
+            <select
+              name="input4"
+              value={formData.input4}
+              onChange={handleInputChange}
+              placeholder="Input 4"
+            >
+              <option value="">Type of Tax</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+            <button type="submit">Start My Tax Return</button>
+          </form>
+          <p className="ptag">
+            By submitting this form I agree to TaxTim's Terms.
+          </p> */}
